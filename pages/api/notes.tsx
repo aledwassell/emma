442a3052ef6fd4414568f82cb549fa2905d5e560
Aled Wassell <aled@phntms.com>
2023-01-19 07:04:12 +0000

    After main pull

---
+++ pages/api/notes.tsx
@@ -0,0 +1,22 @@
+import {NextApiRequest, NextApiResponse} from 'next';
+
+import {collection, getDocs} from 'firebase/firestore';
+import {db} from '../../firestore';
+
+async function getNotes() {
+  const notes = await getDocs(collection(db, 'notes'));
+  return notes.docs.map(doc => ({id: doc.id, ...doc.data()})) as any[];
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    res.json({body: 'posting'});
+  }
+
+  if (req.method === 'GET') {
+    console.log(getNotes());
+    res.json([
+      {asdasdf: 'asdfasdf'}
+    ]);
+  }
+}

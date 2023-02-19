7a92d543211b1511c94c0ac70c27e8baf02759d1
aledwassell <aledwassell@googlemail.com>
2023-02-19 17:16:58 +0000

    Adding the nav bar and refactor links list.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,26 @@
 import Link from 'next/link';
 import styles from '../styles/Nav.module.scss';
 
+interface RouterLink {
+  route: string;
+  routeName: string;
+}
+
+const LINKS = [
+  {route: '/', routeName: 'Home'},
+  {route: '/create', routeName: 'Create'},
+];
+
 export default function Navbar() {
   return (
     <nav className={styles.nav}>
-      <Link href="/">Home</Link>
+      {LINKS?.map((link: RouterLink, index: number) => {
+        return (
+          <Link key={index} href={link.route}>
+            {link.routeName}
+          </Link>
+        );
+      })}
     </nav>
   );
 }
